63f711cca8f54e8d592d9ae35b07c2646a0ad22d
Carlos Kohaute <97993704+CarlosKohaute@users.noreply.github.com>
2022-05-19 23:45:32 -0300

    Commit

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const cells = [
     photo: './assets/images/s22ultra.png',
   },
   {
-    id: 1,
+    id: 4,
     name: 'Xiaomi 12 PRO',
     description:
       'Versão global xiaomi 12 pro smartphone 8gb 256gb/12gb 256gb snapdragon 8 gen 1 núcleo octa 6.73 polegada display 120hz 4600mah 120w',
@@ -39,14 +39,14 @@ const cells = [
     photo: './assets/images/xiaomi12pro.png',
   },
   {
-    id: 2,
+    id: 5,
     name: 'Iphone 13 PRO',
     description: 'Apple iPhone 13 Pro 128GB Prateado Tela 6,1” - 12MP iOS ',
     price: 7800,
     photo: './assets/images/iphone13pro.png',
   },
   {
-    id: 3,
+    id: 6,
     name: 'Galaxy S22 Ultra',
     description:
       "Smartphone Samsung Galaxy S22 Ultra 256GB 5G Wi-Fi Tela 6.8'' Dual Chip 12GB RAM Câmera Quádrupla + Selfie 40MP - Preto ",
